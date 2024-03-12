fec77b7e830ae52e90dcf097897984291e958f88
Witty-Wizard <shashankmarch27@gmail.com>
2024-03-12 17:00:12 +0530

    testing

--- src/serialIO.h
+++ src/serialIO.h
@@ -1,16 +1,70 @@
+/*!
+ * @file serialIO.h
+ *
+ * @mainpage SerialIO
+ * @section intro_sec Introduction
+ * @brief A versatile Arduino library that provides support for decoding multiple RC
+ * protocols.
+ *
+ * SerialIO is a common library designed to simplify the implementation of RC protocol
+ * decoding in Arduino projects. It provides a modular and extensible architecture
+ * that allows for easy integration of various RC protocols.
+ *
+ * @section author Author
+ * Written by Witty Wizard
+ * @section license License
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #pragma once
 #ifndef SERIALIO_H
 #define SERIALIO_H
 #include "crsf/crsf_protocol.h"
 #include <Arduino.h>
 
-class serialIO {
+/**************************************************************************/
+/*!
+    @brief  Class that stores state and functions for initialising and decoding rc protocol
+*/
+/**************************************************************************/
+class serialIO
+{
 public:
   serialIO(HardwareSerial *rxPort, int rxPin, int txPin, bool inverted)
       : _rxPort(rxPort), _rxPin(rxPin), _txPin(txPin), _inverted(inverted){};
+
   virtual ~serialIO(){};
+
+  /**************************************************************************/
+  /*!
+      @brief Initialises the pins and setup serial port.
+  */
+  /**************************************************************************/
   virtual void begin();
+  /**************************************************************************/
+  /*!
+      @brief decode the incoming serial data.
+  */
+  /**************************************************************************/
   virtual void processIncoming();
+  /**************************************************************************/
+  /*!
+      @brief Gives the decoded rc channels
+      @param  channelData
+              A pointer to a crsf_channels_t struct where the decoded channel data will be stored.
+              
+  */
+  /**************************************************************************/
   virtual void getChannel(crsf_channels_t *channelData);
 
 protected:
