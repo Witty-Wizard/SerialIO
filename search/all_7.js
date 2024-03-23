f012fb6d40571213d36409d3fbb5620a372b9edf
Witty-Wizard <Witty-Wizard@users.noreply.github.com>
2024-03-23 20:10:36 +0000

    deploy: 0ed6db1f3ece2ba7922a46d0f847fc6d4033ef97

--- search/all_7.js
+++ search/all_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['license_0',['License',['../index.html#autotoc_md6',1,'']]]
+  ['installation_0',['Installation',['../index.html#autotoc_md4',1,'Arduino Installation'],['../index.html#installation',1,'Installation'],['../index.html#autotoc_md5',1,'PlatformIO Installation']]]
 ];
