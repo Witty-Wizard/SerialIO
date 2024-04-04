e68947631a6b235cb86bcb0bfee779bfab402471
Witty-Wizard <Witty-Wizard@users.noreply.github.com>
2024-04-04 05:04:25 +0000

    deploy: 31aa65a4fd085313633d47cdb5218704244c507c

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['begin_0',['begin',['../classcrsf.html#a86bfe46e0bdbf447885dd1614ed939d0',1,'crsf::begin()'],['../classsbus.html#a7232d82f864fe8cc32b39c0c1989f418',1,'sbus::begin()'],['../classserialIO.html#a5a4eddda14bb5c1a56f8470850ccf409',1,'serialIO::begin()']]]
+  ['begin_0',['begin',['../classcrsf.html#a86bfe46e0bdbf447885dd1614ed939d0',1,'crsf::begin()'],['../classsbus.html#a7232d82f864fe8cc32b39c0c1989f418',1,'sbus::begin()'],['../classSerialIO.html#a8d7b00745bb220b8675c15dbc2f5dcdc',1,'SerialIO::begin()']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['sbus_0',['sbus',['../classsbus.html#a93b49bb867b576c735e31c892e061cff',1,'sbus']]],
-  ['serialio_1',['serialIO',['../classserialIO.html#adcdd78a568e0f27449becb25f74f6530',1,'serialIO']]]
+  ['serialio_1',['SerialIO',['../classSerialIO.html#a0781ef98a22989c46bd7f6dbcfcc37d3',1,'SerialIO']]]
 ];
