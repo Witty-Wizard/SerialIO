bdf0c68083c4e8c4cc465ebf3e6ba23fd48bfe94
Shashank Agarwal <48916902+Witty-Wizard@users.noreply.github.com>
2024-06-16 18:17:16 +0530

    ADDED:Support for Rp2040 HardwareSerial

--- src/crsf.cpp
+++ src/crsf.cpp
@@ -1,10 +1,18 @@
 #include "crsf.h"
-
 void crsf::begin() {
-  HardwareSerial* serialPort = static_cast<HardwareSerial*>(_rxPort);
 
-  // Initialize the serial port
+// Initialize the serial port
+#if defined(ARDUINO_ARCH_ESP32)
+  HardwareSerial *serialPort = (HardwareSerial *)_rxPort;
   serialPort->begin(CRSF_BAUDRATE, SERIAL_8N1, _rxPin, _txPin, _inverted);
+#elif defined(ARDUINO_ARCH_RP2040)
+  SerialUART *serialPort = (SerialUART *)_rxPort;
+  serialPort->setPinout(_txPin, _rxPin);
+  serialPort->begin(CRSF_BAUDRATE, SERIAL_8N1);
+#else
+#error                                                                         \
+    "Unsupported hardware platform. This code supports ESP32 and RP2040 architectures only."
+#endif
 }
 
 void crsf::processIncoming() {
@@ -36,21 +44,21 @@ void crsf::processIncoming() {
 }
 
 void crsf::getChannel(crsf_channels_t *channelData) {
-  if(crc8(&_rxData[2],_rxData[1]) == 0)
+  if (crc8(&_rxData[2], _rxData[1]) == 0)
     memcpy(channelData, &_rxData[3], sizeof(*channelData));
 }
 
 uint8_t crsf::crc8(uint8_t *data, uint8_t len) {
-    uint8_t crc = 0;
-    for (uint8_t i = 0; i < len; i++) {
-        crc ^= data[i];
-        for (uint8_t j = 0; j < 8; j++) {
-            if (crc & 0x80) {
-                crc = (crc << 1) ^ CRC8_POLY_D5;
-            } else {
-                crc <<= 1;
-            }
-        }
+  uint8_t crc = 0;
+  for (uint8_t i = 0; i < len; i++) {
+    crc ^= data[i];
+    for (uint8_t j = 0; j < 8; j++) {
+      if (crc & 0x80) {
+        crc = (crc << 1) ^ CRC8_POLY_D5;
+      } else {
+        crc <<= 1;
+      }
     }
-    return crc;
+  }
+  return crc;
 }
