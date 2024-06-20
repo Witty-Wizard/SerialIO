7f14512459a73450ea50a59a4dd2dbc5abc0dcf4
Shashank Agarwal <48916902+Witty-Wizard@users.noreply.github.com>
2024-06-20 14:02:53 +0530

    [FIXED]:Compilation issue

--- src/SerialIO.cpp
+++ src/SerialIO.cpp
@@ -2,41 +2,45 @@
 
 SerialIO::SerialIO(Stream *rxPort, int rxPin, int txPin, bool inverted)
     : _rxPort(rxPort), _rxPin(rxPin), _txPin(txPin), _inverted(inverted) {
-    // Constructor implementation
+  // Constructor implementation
 }
 
 SerialIO::~SerialIO() {
-  // End serial communication
-  #if defined(ARDUINO_ARCH_ESP32)
+// End serial communication
+#if defined(ARDUINO_ARCH_ESP32)
   HardwareSerial *serialPort = (HardwareSerial *)_rxPort;
   serialPort->end();
-  #elif defined(ARDUINO_ARCH_RP2040)
+#elif defined(ARDUINO_ARCH_RP2040)
   SerialUART *serialPort = (SerialUART *)_rxPort;
   serialPort->end();
-  #else
+#else
+#error                                                                         \
+    "Unsupported hardware platform. This Sbus is supported on ESP32 and RP2040 architectures only."
+#endif
 }
 
 void SerialIO::begin() {
-    // Initialize pins and setup serial port
+  // Initialize pins and setup serial port
 }
 
 void SerialIO::processIncoming() {
-    // Decode the incoming serial data
-    // This function can be empty if actual decoding is implemented in subclasses
+  // Decode the incoming serial data
+  // This function can be empty if actual decoding is implemented in subclasses
 }
 
 void SerialIO::getChannel(crsf_channels_t *channelData) {
-    // Get the decoded RC channels
-    // This function can be empty if actual decoding is implemented in subclasses
+  // Get the decoded RC channels
+  // This function can be empty if actual decoding is implemented in subclasses
 }
 
-void SerialIO::writeChannel(crsf_channels_t channelData){
-    #if defined(ARDUINO_ARCH_ESP32)
-    HardwareSerial *serialPort = (HardwareSerial *)_rxPort;
-    #elif defined(ARDUINO_ARCH_RP2040)
-    SerialUART *serialPort = (SerialUART *)_rxPort;
-    #else
-    memcpy(buffer, (uint8_t*)&channelData, sizeof(channelData));
-    serialPort->write((uint8_t*)&channelData, sizeof(channelData));
-    free(buffer);
+void SerialIO::writeChannel(crsf_channels_t channelData) {
+#if defined(ARDUINO_ARCH_ESP32)
+  HardwareSerial *serialPort = (HardwareSerial *)_rxPort;
+#elif defined(ARDUINO_ARCH_RP2040)
+  SerialUART *serialPort = (SerialUART *)_rxPort;
+#else
+#error                                                                         \
+    "Unsupported hardware platform. This Sbus is supported on ESP32 and RP2040 architectures only."
+#endif
+  serialPort->write((uint8_t *)&channelData, sizeof(channelData));
 }
--- src/SerialIO.h
+++ src/SerialIO.h
@@ -14,7 +14,8 @@
    rc protocol
 */
 /**************************************************************************/
-class SerialIO {
+class SerialIO
+{
 public:
   /**
    * @brief Constructor for the SerialIO class.
@@ -29,7 +30,7 @@ public:
    */
   SerialIO(Stream *rxPort, int rxPin, int txPin, bool inverted);
 
-  virtual ~SerialIO(){};
+  virtual ~SerialIO();
 
   /**************************************************************************/
   /*!
@@ -58,14 +59,14 @@ public:
 
 protected:
   Stream
-      *_rxPort; // Pointer to the hardware serial port used for communication.
+      *_rxPort;         // Pointer to the hardware serial port used for communication.
   bool _headerDetected; // Flag indicating whether a header has been detected
                         // in the incoming data.
-  bool _inverted;   // Indicates whether the serial signal is inverted (true) or
-                    // not (false).
-  uint8_t _rxIndex; // Index for the receive_buffer.
-  int _rxPin;       // The RX pin number.
-  int _txPin;       // The TX pin number.
+  bool _inverted;       // Indicates whether the serial signal is inverted (true) or
+                        // not (false).
+  uint8_t _rxIndex;     // Index for the receive_buffer.
+  int _rxPin;           // The RX pin number.
+  int _txPin;           // The TX pin number.
   uint8_t _buffer;
   uint8_t _prevBuffer;
 };
