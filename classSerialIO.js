56134dfd0e2a645150c2937fa2598cbe58a243ee
Witty-Wizard <Witty-Wizard@users.noreply.github.com>
2024-06-21 08:51:15 +0000

    deploy: d13b2af2625721d80d209c3483cc9c983de60ace

--- classSerialIO.js
+++ classSerialIO.js
@@ -1,7 +1,7 @@
 var classSerialIO =
 [
     [ "SerialIO", "classSerialIO.html#a51676a51e38a07822efcc9ffc3cd2677", null ],
-    [ "begin", "classSerialIO.html#a30590d6081a2c5a86b06564b4a9f149a", null ],
+    [ "begin", "classSerialIO.html#a1a5b10d8a45d1ef0c1eeb83615c3cb5d", null ],
     [ "getChannel", "classSerialIO.html#af9ad40f8b521c431a0bf5a3ef91a4dc6", null ],
-    [ "processIncoming", "classSerialIO.html#a3bcf156bb4d6f43f9e53b87cdb1d1fd4", null ]
+    [ "processIncoming", "classSerialIO.html#a3ef00c98b6734f69e3245d86c36949ce", null ]
 ];
--- search/all_1.js
+++ search/all_1.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['basic_20example_0',['Basic Example',['../index.html#autotoc_md7',1,'CRSF Basic Example'],['../index.html#autotoc_md8',1,'SBUS Basic Example']]],
   ['basic_20functions_20required_1',['Basic Functions Required',['../index.html#autotoc_md11',1,'']]],
-  ['begin_2',['begin',['../classcrsf.html#a86bfe46e0bdbf447885dd1614ed939d0',1,'crsf::begin()'],['../classibus.html#a9edf9871632e0f5b2a7e6e7ee0ff128b',1,'ibus::begin()'],['../classsbus.html#a7232d82f864fe8cc32b39c0c1989f418',1,'sbus::begin()'],['../classSerialIO.html#a30590d6081a2c5a86b06564b4a9f149a',1,'SerialIO::begin()']]]
+  ['begin_2',['begin',['../classcrsf.html#a86bfe46e0bdbf447885dd1614ed939d0',1,'crsf::begin()'],['../classibus.html#a9edf9871632e0f5b2a7e6e7ee0ff128b',1,'ibus::begin()'],['../classsbus.html#a7232d82f864fe8cc32b39c0c1989f418',1,'sbus::begin()'],['../classSerialIO.html#a1a5b10d8a45d1ef0c1eeb83615c3cb5d',1,'SerialIO::begin()']]]
 ];
