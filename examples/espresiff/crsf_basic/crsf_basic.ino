3dd327b1a995044ce639a40f55ad78e726dc28cd
Shashank Agarwal <48916902+Witty-Wizard@users.noreply.github.com>
2024-06-22 12:48:22 +0530

    [Added]: Examples for avr and espresiff

---
+++ examples/espresiff/crsf_basic/crsf_basic.ino
@@ -0,0 +1,29 @@
+/*!
+ * @file crsf_basic.ino
+ */
+#include <SerialIO.h>
+#define RX_PIN 16
+
+crsf_channels_t channelData;
+
+crsf receiver(&Serial1,RX_PIN);
+
+void setup() { 
+  receiver.begin();
+  Serial.being(9600);
+}
+
+void loop() {
+  receiver.processIncoming();
+  receiver.getChannel(&channelData);
+
+  Serial.print("Channel 1: ");
+  Serial.print(channelData.channel1);
+  Serial.print(" Channel 2: ");
+  Serial.print(channelData.channel2);
+  Serial.print(" Channel 3: ");
+  Serial.print(channelData.channel3);
+  Serial.print(" Channel 4: ");
+  Serial.println(channelData.channel4);
+
+}
