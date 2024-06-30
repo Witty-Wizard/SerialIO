dd1a7fbc9bc64f25b9c6517bb5d3262c230483fe
Shashank Agarwal <48916902+Witty-Wizard@users.noreply.github.com>
2024-06-30 06:30:15 +0000

    fixed error

--- src/SerialIO.h
+++ src/SerialIO.h
@@ -63,8 +63,8 @@ protected:
   uint8_t _buffer;
   uint8_t _prevBuffer;
 };
-#include "crsf.h"
-#include "ibus.h"
-#include "sbus.h"
+#include "crsf/crsf.h"
+#include "ibus/ibus.h"
+#include "sbus/sbus.h"
 
 #endif
