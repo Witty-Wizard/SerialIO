b6a510f7ce03a34b1ab6b0e894b3947209caf5d0
Witty-Wizard <agarwalshashank429+github@gmail.com>
2024-12-07 13:21:00 +0530

    Added: yml file to ignore the CI for rp2040

--- examples/rp2040/ibus_basic/ibus_basic.ino
+++ examples/rp2040/ibus_basic/ibus_basic.ino
@@ -1,7 +1,8 @@
 /*!
- * @file crsf_basic.ino
+ * @file ibus_basic.ino
  */
 /*
+
 # Sample platformio.ini file:
 # ---------------------------
 [platformio]
@@ -17,16 +18,17 @@ monitor_speed = 115200
 lib_deps =
   https://github.com/Witty-Wizard/SerialIO
 */
+
 #include <SerialIO.h>
 
 #define SBUS_TX_PIN 0
 #define SBUS_RX_PIN 1
 
 rc_channels_t rcdata;
-ibus receiver(&Serial1, SBUS_RX_PIN, SBUS_TX_PIN, true); // RP2040 requires the TX_PIN so to not hang up the mcu
+ibus receiver(&Serial1, SBUS_RX_PIN, SBUS_TX_PIN,
+              true); // RP2040 requires the TX_PIN so to not hang up the mcu
 
-void setup()
-{
+void setup() {
   // setup sbus receiver
   Serial1.setInvertRX(true); // will only work with the earlephilhower core
   Serial1.setInvertTX(true);
@@ -35,19 +37,18 @@ void setup()
   Serial.begin(115200);
 }
 
-void loop()
-{
+void loop() {
   static unsigned long last_millis = millis();
 
   receiver.processIncoming();
   receiver.getChannel(&rcdata);
 
-  if (millis() > last_millis + 100)
-  {
+  if (millis() > last_millis + 100) {
     Serial.printf("RC: %4d %4d %4d %4d %4d %4d %4d %4d %4d %4d %4d %4d %4d %4d",
-                  rcdata.channel1, rcdata.channel2, rcdata.channel3, rcdata.channel4,
-                  rcdata.channel5, rcdata.channel6, rcdata.channel7, rcdata.channel8,
-                  rcdata.channel9, rcdata.channel10, rcdata.channel11, rcdata.channel12,
+                  rcdata.channel1, rcdata.channel2, rcdata.channel3,
+                  rcdata.channel4, rcdata.channel5, rcdata.channel6,
+                  rcdata.channel7, rcdata.channel8, rcdata.channel9,
+                  rcdata.channel10, rcdata.channel11, rcdata.channel12,
                   rcdata.channel13, rcdata.channel14);
     Serial.println();
     last_millis = millis();
