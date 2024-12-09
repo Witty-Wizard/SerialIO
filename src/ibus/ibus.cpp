b73a49e60e17eb24ed7c37f13326404db685aea5
Shashank Agarwal <48916902+Witty-Wizard@users.noreply.github.com>
2024-12-09 09:44:47 -0800

    Added: Loop to decode data form array to struct

--- src/ibus/ibus.cpp
+++ src/ibus/ibus.cpp
@@ -28,22 +28,11 @@ void ibus::processIncoming() {
     _rxData[IBUS_MAX_PACKET_SIZE - 1] = _rxPort->read();
     if (_rxData[0] == IBUS_HEADER1 && _rxData[1] == IBUS_HEADER2) {
       if (checkSum()) {
-        _channelData.header = (_rxData[1] << 8) | _rxData[0];
-        _channelData.channel1 = (_rxData[3] << 8) | _rxData[2];
-        _channelData.channel2 = (_rxData[5] << 8) | _rxData[4];
-        _channelData.channel3 = (_rxData[7] << 8) | _rxData[6];
-        _channelData.channel4 = (_rxData[9] << 8) | _rxData[8];
-        _channelData.channel5 = (_rxData[11] << 8) | _rxData[10];
-        _channelData.channel6 = (_rxData[13] << 8) | _rxData[12];
-        _channelData.channel7 = (_rxData[15] << 8) | _rxData[14];
-        _channelData.channel8 = (_rxData[17] << 8) | _rxData[16];
-        _channelData.channel9 = (_rxData[19] << 8) | _rxData[18];
-        _channelData.channel10 = (_rxData[21] << 8) | _rxData[20];
-        _channelData.channel11 = (_rxData[23] << 8) | _rxData[22];
-        _channelData.channel12 = (_rxData[25] << 8) | _rxData[24];
-        _channelData.channel13 = (_rxData[27] << 8) | _rxData[26];
-        _channelData.channel14 = (_rxData[29] << 8) | _rxData[28];
-        _channelData.checksum = (_rxData[31] << 8) | _rxData[30];
+        size_t len = sizeof(ibus_channels_s) / sizeof(uint16_t);
+        uint16_t *arr = (uint16_t *)malloc(len * sizeof(uint16_t));
+        for(int i = 0; i < len; i++){
+          arr[i] = (_rxData[i*sizeof(uint16_t)+1] << 8) | _rxData[i*sizeof(uint16_t)];
+        }
       }
     }
     leftShift(_rxData, sizeof(_rxData));
