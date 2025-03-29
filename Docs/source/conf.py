04b9b137d3f7348b3082572cd6c9e441b1dc5448
Witty-Wizard <agarwalshashank429+github@gmail.com>
2025-03-30 00:31:40 +0530

    improved documentation

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -6,33 +6,28 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'SerialIO'
-copyright = '2025, Witty-Wizard'
-author = 'Witty-Wizard'
-release = '0.2.4'
+project = "SerialIO"
+copyright = "2025, Witty-Wizard"
+author = "Witty-Wizard"
+release = "0.2.4"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'breathe'
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "breathe"]
 
-templates_path = ['_templates']
+
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 
 # -- Breathe configurations --------------------------------------------------
 # Path to the generated Doxygen XML files
-breathe_projects = {
-    "SerialIO": "../xml"
-}
+breathe_projects = {"SerialIO": "../xml"}
 breathe_default_project = "SerialIO"
-
