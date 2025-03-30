f106e37190bed44ce4616ca08c8841cfeed7436f
Witty-Wizard <agarwalshashank429+github@gmail.com>
2025-03-30 13:49:26 +0530

    Added: Contents and copy button

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -14,7 +14,7 @@ release = "0.2.4"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "breathe"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "breathe", "sphinx_copybutton"]
 
 
 templates_path = ["_templates"]
---
+++ Docs/source/example.rst
@@ -0,0 +1,16 @@
+.. _example:
+
+Examples
+========
+
+CRSF Basic Example
+------------------
+
+.. literalinclude:: ../../examples/espresiff/crsf_basic/crsf_basic.ino
+   :language: cpp
+
+SBUS Basic Example
+------------------
+
+.. literalinclude:: ../../examples/espresiff/sbus_basic/sbus_basic.ino
+   :language: cpp
